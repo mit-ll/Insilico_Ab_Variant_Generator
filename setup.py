2351972123906e24a96810a9844698acf05b9376
lshinggg <87205317+lshinggg@users.noreply.github.com>
2021-11-15 09:38:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(name='insilico_ab_lib',
         version='1.0',
-        description='A script to generate in silico antbody sequence variants',
+        description='A script to generate in silico antibody sequence variants',
         url='--',
         author='MIT Lincoln Laboratory,
         license='BSD-2-Clause',
