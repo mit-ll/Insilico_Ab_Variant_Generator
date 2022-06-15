a6f46d1016eeaecd537dfee79c9ff595a30c938b
lshinggg <87205317+lshinggg@users.noreply.github.com>
2022-06-15 10:59:02 -0400

    Update run_library_gen.py

--- scripts/run_library_gen.py
+++ scripts/run_library_gen.py
@@ -3,7 +3,6 @@
     __description__ = "Main script to initiate config/command line argument parsing"
     __copyright__= "© 2021 MASSACHUSETTS INSTITUTE OF TECHNOLOGY"
     __disclaimer__="THE SOFTWARE/FIRMWARE IS PROVIDED TO YOU ON AN “AS-IS” BASIS."
-    __patent_ownership__="Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014)"
     __SPDX_License_Identifier__="BSD-2-Clause"
 
 '''
