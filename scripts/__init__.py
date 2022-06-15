b3c72efb38cffd04dd8f92349480ae52a4432ae8
lshinggg <87205317+lshinggg@users.noreply.github.com>
2022-06-15 10:59:16 -0400

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,7 +2,6 @@
     __version__="1.0"
     __copyright__= "© 2021 MASSACHUSETTS INSTITUTE OF TECHNOLOGY"
     __disclaimer__="THE SOFTWARE/FIRMWARE IS PROVIDED TO YOU ON AN “AS-IS” BASIS."
-    __patent_ownership__="Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014)"
     __SPDX_License_Identifier__="BSD-2-Clause"
 '''
 
