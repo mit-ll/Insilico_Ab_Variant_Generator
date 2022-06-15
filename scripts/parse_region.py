44f48909e1c6032ac74f9ed5b8f504a2088e526a
lshinggg <87205317+lshinggg@users.noreply.github.com>
2022-06-15 10:58:30 -0400

    Update parse_region.py

--- scripts/parse_region.py
+++ scripts/parse_region.py
@@ -6,7 +6,6 @@
 
     __disclaimer__="THE SOFTWARE/FIRMWARE IS PROVIDED TO YOU ON AN “AS-IS” BASIS."
 
-    __patent_ownership__="Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014)"
     __SPDX_License_Identifier__="BSD-2-Clause"
 '''
 
